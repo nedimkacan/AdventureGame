9a47522ec3730c77a6be80d02e96cdf80ff1be64
Nedim Kaçan <ndmkcn1453@gmail.com>
2023-06-20 18:31:40 +0300

    Inventory class have been created.

---
+++ src/main/java/org/example/Inventory.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Inventory {
+    String weaponName;
+    String armorName;
+    int weaponDamage;
+    int armorDefence;
+    boolean water;
+    boolean food;
+    boolean firewoord;
+}
