3a28ff3e345a264eda3a120dc57497dc4f144961
Nedim Kaçan <ndmkcn1453@gmail.com>
2023-06-20 18:31:23 +0300

    Game class have been created.

---
+++ src/main/java/org/example/games/Game.java
@@ -0,0 +1,19 @@
+package org.example.games;
+
+import org.example.players.Player;
+
+import java.util.Scanner;
+
+public class Game {
+    private Scanner input=new Scanner(System.in);
+    public void start(){
+        System.out.println("Macera başladı !");
+        System.out.print("Oyuncu Adı: ");
+        String playerName=input.nextLine();
+        Player player=new Player(playerName);
+        System.out.println("Sayın " + player.getName() + " bu karanlık ve puslu adaya hoşgeldiniz !");
+        System.out.println("Bu puslu ve karanlık adada yaşanan olaylar tümüyle gerçektir !");
+        player.selectCharacter();
+
+    }
+}
